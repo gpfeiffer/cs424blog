55e81dcf6d6e3e2fa74036bcb4555aacdb1ae292
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-09-01 11:17:17 +0100

    generated article scaffold

---
+++ db/migrate/20130901101614_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.date :published_on
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
