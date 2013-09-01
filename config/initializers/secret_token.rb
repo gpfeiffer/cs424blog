257c33a6bb16d4b68da9a64308520ebeb58f1f38
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-09-01 11:12:25 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headblog::Application.config.secret_token = 'e17e7dfa0e1dd3972645830acb643dbef5aae8bf270d229e23e59e8e49fb4b7d8652ce757bc6ec0a1c0d3dd5e6dfc0c7c2b578d8f5fd124e35e12cd8b2968ea0'
