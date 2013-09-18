96ee7ba531e169cfadd3936baed2b1e3fc284895
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-09-18 21:01:33 +0100

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ Headblog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # mailer settings.
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "nuigalway.ie",
+    :authentication => "plain",
+    :user_name => ENV["USERNAME"],
+    :password => ENV["PASSWORD"],
+    :enable_starttls_auto => true
+  }
 end
